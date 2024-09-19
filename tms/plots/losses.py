8687495322a8ba694d125df9fb12a9b385868878
Tassilo Neubauer <46806445+sonofhypnos@users.noreply.github.com>
2024-09-19 19:04:54 +0200

    Update the plotting function

--- tms/plots/losses.py
+++ tms/plots/losses.py
@@ -18,42 +18,39 @@ def plot_results_by_indices(results, indices):
         The maximum number of plots to display, by default 5.
     """
     
-    sparsity = [result['parameters']['sparsity'] for result in results]
-    for sparse_value in sparsity:
-        print(f"Plot polygons for sparsity={sparse_value}")
-        for index in indices:
-            STEPS = results[index]['parameters']['log_ivl']
-            if results[index]['parameters']['sparsity'] != sparse_value:
-                continue
-            logs = results[index]['logs']
+    for index in indices:
+        sparse_value = results[index]['parameters']['sparsity']
+        STEPS = results[index]['parameters']['log_ivl']
+        logs = results[index]['logs']
 
-            losses = [logs.loc[logs['step'] == s, 'loss'].values[0] for s in STEPS]
+        losses = [logs.loc[logs['step'] == s, 'loss'].values[0] for s in STEPS]
 
-            NUM_EPOCHS = results[index]['parameters']['num_epochs']
-            PLOT_STEPS = [min(STEPS, key=lambda s: abs(s-i)) for i in [0, 200, 2000, 10000, NUM_EPOCHS - 1]]
-            PLOT_INDICES = [STEPS.index(s) for s in PLOT_STEPS]
-            Ws = [results[index]['weights'][i]['embedding.weight'] for i in PLOT_INDICES]
-            biases = [results[index]['weights'][i]['unembedding.bias'] for i in PLOT_INDICES]
-            model = ToyAutoencoder(6, 2, final_bias=True)
-            new_weights = {}
-            for idx, ndarray in results[index]['weights'][PLOT_INDICES[-1]].items():
-                new_weights[idx] = torch.from_numpy(ndarray)
+        NUM_EPOCHS = results[index]['parameters']['num_epochs']
+        PLOT_STEPS = [min(STEPS, key=lambda s: abs(s-i)) for i in [0, 200, 2000, 10000, NUM_EPOCHS - 1]]
+        PLOT_INDICES = [STEPS.index(s) for s in PLOT_STEPS]
+        Ws = [results[index]['weights'][i]['embedding.weight'] for i in PLOT_INDICES]
+        biases = [results[index]['weights'][i]['unembedding.bias'] for i in PLOT_INDICES]
+        model = ToyAutoencoder(6, 2, final_bias=True)
+        new_weights = {}
+        for idx, ndarray in results[index]['weights'][PLOT_INDICES[-1]].items():
+            new_weights[idx] = torch.from_numpy(ndarray)
 
-            criterion = nn.MSELoss()
-        
-            model.load_state_dict(new_weights)
+        criterion = nn.MSELoss()
+    
+        model.load_state_dict(new_weights)
 
-            test_set = SyntheticBinaryValued(10000, 6, sparse_value)
-            mean_loss_test = 0
-            for sample in test_set:
-                output = model(sample)
-                mean_loss_test += criterion(output, sample)
-            # print("Mean loss test:")
-            print(f"index: {index}")
-            print(mean_loss_test/10000)
-
-            plot_losses_and_polygons(STEPS, losses, PLOT_STEPS, Ws, biases)
-            plt.show()
+        test_set = SyntheticBinaryValued(10000, 6, sparse_value)
+        mean_loss_test = 0
+        for sample in test_set:
+            output = model(sample)
+            mean_loss_test += criterion(output, sample)
+        # print("Mean loss test:")
+        print(f"index: {index}")
+        print(mean_loss_test/10000)
+    
+        model.load_state_dict(new_weights)
+        plot_losses_and_polygons(STEPS, losses, PLOT_STEPS, Ws, biases)
+        plt.show()
             
 
 def plot_results(results, plot_number=5):
